a6af0256679a9e356fb8a9cb6875d8f87f2d5cf2
Keith Watford <keith.l.watford@gmail.com>
2021-06-15 22:05:31 -0400

    got the UV index colors working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 var APIKey = "ebc561a4f7b5280398d199954c0c6462";
-var userCity = "Philadelphia"
+var userCity = "Boston"
 
 // document.querySelector("#search").value.trim;
 console.log(userCity);
@@ -10,8 +10,6 @@ var today = moment().format('MM-DD-YYYY');
 var submitButton = document.getElementsByClassName("btn");
 
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + userCity + "&appid=" + APIKey;
-
-
 
 // function getWeather(event) {
 //   event.preventDefault();
@@ -31,15 +29,12 @@ fetch(queryURL)
       fetch(uvURL)
         .then((res) => res.json()
           .then((data) => {
-            console.log(data);
+            // console.log(data);
             // write jquery here with dynamic elements.
             // console.log(data.current.temp);
             // console.log(data.current.uvi);
             // console.log(data.current.wind_speed);
             // console.log(data.current.humidity);
-
-            // console.log(data.lat);
-            // console.log(data.lon);
 
             var tempKelvin = data.current.temp
             var tempFahrenheit = Math.floor((tempKelvin - 273.15) * (9 / 5) + 32)
@@ -59,7 +54,33 @@ fetch(queryURL)
             tempEl.textContent = "Temp: " + tempFahrenheit + "°";
             windEl.textContent = "Wind: " + windNum + " MPH"
             humidityEl.textContent = "Humidity: " + humidityNum + "%"
-            uviEl.textContent = "UV Index: " + uviNum;
+            uviEl.textContent = uviNum;
+
+
+
+            function todayUviFavorable() {
+              var sunStrength = document.querySelector("#city-uvi");
+              sunStrength.className += " bg-success";
+            }
+
+            function todayUviModerate() {
+              var sunStrength = document.querySelector("#city-uvi");
+              sunStrength.className += " bg-warning";
+            }
+
+            function todayUviSevere() {
+              var sunStrength = document.querySelector("#city-uvi");
+              sunStrength.className += " bg-danger";
+            }
+
+            if (uviNum <= 2.99) {
+              todayUviFavorable();
+            } else if (uviNum >= 3 && uviNum <= 5.99) {
+              todayUviModerate();
+            } else {
+              todayUviSevere();
+            }
+
 
             // 
             // 
@@ -71,11 +92,11 @@ fetch(queryURL)
             var windNumOne = data.daily[1].wind_speed
             var humidityNumOne = data.daily[1].humidity
 
-            console.log(todayPlusOne);
-            console.log(tempFahrenheitOne);
-            console.log(uviNumOne);
-            console.log(windNumOne);
-            console.log(humidityNumOne);
+            // console.log(todayPlusOne);
+            // console.log(tempFahrenheitOne);
+            // console.log(uviNumOne);
+            // console.log(windNumOne);
+            // console.log(humidityNumOne);
 
             var dateElOne = document.querySelector("#today-1");
             var tempElOne = document.querySelector("#city-temp-1");
@@ -87,7 +108,30 @@ fetch(queryURL)
             tempElOne.textContent = "Temp: " + tempFahrenheitOne + "°";
             windElOne.textContent = "Wind: " + windNumOne + " MPH"
             humidityElOne.textContent = "Humidity: " + humidityNumOne + "%"
-            uviElOne.textContent = "UV Index: " + uviNumOne;
+            uviElOne.textContent = uviNumOne;
+
+            function oneUviFavorable() {
+              var sunStrengthOne = document.querySelector("#city-uvi-1");
+              sunStrengthOne.className += " bg-success";
+            }
+
+            function oneUviModerate() {
+              var sunStrengthOne = document.querySelector("#city-uvi-1");
+              sunStrengthOne.className += " bg-warning";
+            }
+
+            function oneUviSevere() {
+              var sunStrengthOne = document.querySelector("#city-uvi-1");
+              sunStrengthOne.className += " bg-danger";
+            }
+
+            if (uviNumOne <= 2.99) {
+              oneUviFavorable();
+            } else if (uviNumOne >= 3 && uviNumOne <= 5.99) {
+              oneUviModerate();
+            } else {
+              oneUviSevere();
+            }
 
             // 
             // 
@@ -99,11 +143,11 @@ fetch(queryURL)
             var windNumTwo = data.daily[2].wind_speed
             var humidityNumTwo = data.daily[2].humidity
 
-            console.log(todayPlusTwo);
-            console.log(tempFahrenheitTwo);
-            console.log(uviNumTwo);
-            console.log(windNumTwo);
-            console.log(humidityNumTwo);
+            // console.log(todayPlusTwo);
+            // console.log(tempFahrenheitTwo);
+            // console.log(uviNumTwo);
+            // console.log(windNumTwo);
+            // console.log(humidityNumTwo);
 
             var dateElTwo = document.querySelector("#today-2");
             var tempElTwo = document.querySelector("#city-temp-2");
@@ -115,7 +159,30 @@ fetch(queryURL)
             tempElTwo.textContent = "Temp: " + tempFahrenheitTwo + "°";
             windElTwo.textContent = "Wind: " + windNumTwo + " MPH"
             humidityElTwo.textContent = "Humidity: " + humidityNumTwo + "%"
-            uviElTwo.textContent = "UV Index: " + uviNumTwo;
+            uviElTwo.textContent = uviNumTwo;
+
+            function twoUviFavorable() {
+              var sunStrength = document.querySelector("#city-uvi-2");
+              sunStrength.className += " bg-success";
+            }
+
+            function twoUviModerate() {
+              var sunStrength = document.querySelector("#city-uvi-2");
+              sunStrength.className += " bg-warning";
+            }
+
+            function twoUviSevere() {
+              var sunStrength = document.querySelector("#city-uvi-2");
+              sunStrength.className += " bg-danger";
+            }
+
+            if (uviNumTwo <= 2.99) {
+              twoUviFavorable();
+            } else if (uviNumTwo >= 3 && uviNumTwo <= 5.99) {
+              twoUviModerate();
+            } else {
+              twoUviSevere();
+            }
 
             // 
             // 
@@ -127,11 +194,11 @@ fetch(queryURL)
             var windNumThree = data.daily[3].wind_speed
             var humidityNumThree = data.daily[3].humidity
 
-            console.log(todayPlusThree);
-            console.log(tempFahrenheitThree);
-            console.log(uviNumThree);
-            console.log(windNumThree);
-            console.log(humidityNumThree);
+            // console.log(todayPlusThree);
+            // console.log(tempFahrenheitThree);
+            // console.log(uviNumThree);
+            // console.log(windNumThree);
+            // console.log(humidityNumThree);
 
             var dateElThree = document.querySelector("#today-3");
             var tempElThree = document.querySelector("#city-temp-3");
@@ -143,7 +210,30 @@ fetch(queryURL)
             tempElThree.textContent = "Temp: " + tempFahrenheitThree + "°";
             windElThree.textContent = "Wind: " + windNumThree + " MPH"
             humidityElThree.textContent = "Humidity: " + humidityNumThree + "%"
-            uviElThree.textContent = "UV Index: " + uviNumThree;
+            uviElThree.textContent = uviNumThree;
+
+            function threeUviFavorable() {
+              var sunStrength = document.querySelector("#city-uvi-3");
+              sunStrength.className += " bg-success";
+            }
+
+            function threeUviModerate() {
+              var sunStrength = document.querySelector("#city-uvi-3");
+              sunStrength.className += " bg-warning";
+            }
+
+            function threeUviSevere() {
+              var sunStrength = document.querySelector("#city-uvi-3");
+              sunStrength.className += " bg-danger";
+            }
+
+            if (uviNumThree <= 2.99) {
+              threeUviFavorable();
+            } else if (uviNumThree >= 3 && uviNumThree <= 5.99) {
+              threeUviModerate();
+            } else {
+              threeUviSevere();
+            }
 
             // 
             // 
@@ -155,11 +245,11 @@ fetch(queryURL)
             var windNumFour = data.daily[4].wind_speed
             var humidityNumFour = data.daily[4].humidity
 
-            console.log(todayPlusFour);
-            console.log(tempFahrenheitFour);
-            console.log(uviNumFour);
-            console.log(windNumFour);
-            console.log(humidityNumFour);
+            // console.log(todayPlusFour);
+            // console.log(tempFahrenheitFour);
+            // console.log(uviNumFour);
+            // console.log(windNumFour);
+            // console.log(humidityNumFour);
 
             var dateElFour = document.querySelector("#today-4");
             var tempElFour = document.querySelector("#city-temp-4");
@@ -171,7 +261,30 @@ fetch(queryURL)
             tempElFour.textContent = "Temp: " + tempFahrenheitFour + "°";
             windElFour.textContent = "Wind: " + windNumFour + " MPH"
             humidityElFour.textContent = "Humidity: " + humidityNumFour + "%"
-            uviElFour.textContent = "UV Index: " + uviNumFour;
+            uviElFour.textContent = uviNumFour;
+
+            function fourUviFavorable() {
+              var sunStrength = document.querySelector("#city-uvi-4");
+              sunStrength.className += " bg-success";
+            }
+
+            function fourUviModerate() {
+              var sunStrength = document.querySelector("#city-uvi-4");
+              sunStrength.className += " bg-warning";
+            }
+
+            function fourUviSevere() {
+              var sunStrength = document.querySelector("#city-uvi-4");
+              sunStrength.className += " bg-danger";
+            }
+
+            if (uviNumFour <= 2.99) {
+              fourUviFavorable();
+            } else if (uviNumFour >= 3 && uviNumFour <= 5.99) {
+              fourUviModerate();
+            } else {
+              fourUviSevere();
+            }
 
             // 
             // 
@@ -183,11 +296,11 @@ fetch(queryURL)
             var windNumFive = data.daily[5].wind_speed
             var humidityNumFive = data.daily[5].humidity
 
-            console.log(todayPlusFive);
-            console.log(tempFahrenheitFive);
-            console.log(uviNumFive);
-            console.log(windNumFive);
-            console.log(humidityNumFive);
+            // console.log(todayPlusFive);
+            // console.log(tempFahrenheitFive);
+            // console.log(uviNumFive);
+            // console.log(windNumFive);
+            // console.log(humidityNumFive);
 
             var dateElFive = document.querySelector("#today-5");
             var tempElFive = document.querySelector("#city-temp-5");
@@ -199,7 +312,30 @@ fetch(queryURL)
             tempElFive.textContent = "Temp: " + tempFahrenheitFive + "°";
             windElFive.textContent = "Wind: " + windNumFive + " MPH"
             humidityElFive.textContent = "Humidity: " + humidityNumFive + "%"
-            uviElFive.textContent = "UV Index: " + uviNumFive;
+            uviElFive.textContent = uviNumFive;
+
+            function fiveUviFavorable() {
+              var sunStrength = document.querySelector("#city-uvi-5");
+              sunStrength.className += " bg-success";
+            }
+
+            function fiveUviModerate() {
+              var sunStrength = document.querySelector("#city-uvi-5");
+              sunStrength.className += " bg-warning";
+            }
+
+            function fiveUviSevere() {
+              var sunStrength = document.querySelector("#city-uvi-5");
+              sunStrength.className += " bg-danger";
+            }
+
+            if (uviNumFive <= 2.99) {
+              fiveUviFavorable();
+            } else if (uviNumFive >= 3 && uviNumFive <= 5.99) {
+              fiveUviModerate();
+            } else {
+              fiveUviSevere();
+            }
 
             // 
             // 
@@ -215,11 +351,11 @@ fetch(queryURL)
 
 // submitButton.addEventListener('submit', get);
 // WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
+// THEN*** I am presented with current and future conditions for that city and that city is added to the search history
 
 
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, 
+// WHEN*** I view current weather conditions for that city
+// THEN* I am presented with the city name, the date, 
 // an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
 
 
@@ -227,8 +363,8 @@ fetch(queryURL)
 // THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
 
 
-// WHEN I view futur e weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, 
+// WHEN*** I view futur e weather conditions for that city
+// THEN*** I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, 
 //  the temperature, the wind speed, and the humidity
 
 
